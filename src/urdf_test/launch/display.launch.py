d78f5bf2e85de20b46ce013fb8aadb90fb5dd118
Rémi Ait-Younes <remi@ait-younes.fr>
2026-06-03 20:44:48 +0200

    ros: add urdf_test pkg

---
+++ src/urdf_test/launch/display.launch.py
@@ -0,0 +1,44 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import FileContent, LaunchConfiguration, PathJoinSubstitution
+from launch.conditions import IfCondition, UnlessCondition
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+def generate_launch_description():
+    # pkg_path = launch_ros.substitutions.FindPackageShare()
+    urdf = FileContent(
+        PathJoinSubstitution([FindPackageShare('urdf_test'), 'urdf', 'model.urdf']))
+
+    return LaunchDescription([
+       DeclareLaunchArgument(
+            'joint_gui',
+            default_value='True',
+            description='Flag to start joint_state_publisher_gui'),
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{'robot_description': urdf}],
+        ) ,
+        Node(
+            package='joint_state_publisher',
+            executable='joint_state_publisher',
+            name='joint_state_publisher',
+            parameters=[{'robot_description': urdf}],
+            condition=UnlessCondition(LaunchConfiguration('joint_gui')),
+        ),
+        Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+            name='joint_state_publisher_gui',
+            condition=IfCondition(LaunchConfiguration('joint_gui')),
+        ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen',
+        ),
+    ])
